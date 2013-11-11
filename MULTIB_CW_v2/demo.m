5efd7014ff1852a932678707db1d714997161e5d
Guosheng <guosheng@guosheng-P55A-UD3.(none)>
2013-11-11 13:50:14 +1030

    changes

--- MULTIB_CW_v2/demo.m
+++ MULTIB_CW_v2/demo.m
@@ -1,16 +1,16 @@
 
 
-% code author: Guosheng Lin
-% contact: guosheng.lin@gmail.com or guosheng.lin@adelaide.edu.au
+% Code author: Guosheng Lin
+% Contact: guosheng.lin@gmail.com or guosheng.lin@adelaide.edu.au
 
 % this code is for the following paper, please cite:
 % [1] Guosheng Lin, Chunhua Shen, Anton van den Hengel and David Suter;
 % "Fast training of effective multi-class boosting using coordinate descent optimization", ACCV2012.
 
-% note that this code is an improved and simplified version of our accv paper.
+% Note that this code is an improved and simplified version of our ACCV paper.
  
 
-% this code also use the following code for decision tree implementation:
+% The following code for decision tree implementation is included:
 % Piotr's Image & Video Matlab Toolbox
 % http://vision.ucsd.edu/~pdollar/toolbox/doc/
 % please cite their paper:
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
  code author: Guosheng Lin
  contact: guosheng.lin@gmail.com or guosheng.lin@adelaide.edu.au
 
- This code is an improved and simplified version of our accv paper. Please cite:
+ This code is for our ACCV paper. Please cite:
  [1] Guosheng Lin, Chunhua Shen, Anton van den Hengel and David Suter;
  "Fast training of effective multi-class boosting using coordinate descent optimization", ACCV2012.
 
@@ -9,7 +9,7 @@
  We also use the following code for decision tree implementation:
  Piotr's Image & Video Matlab Toolbox
  http://vision.ucsd.edu/~pdollar/toolbox/doc/
- please cite their paper:
+ Please cite their paper:
  [2] R. Appel, T. Fuchs, P. Dollár, P. Perona; 
  "Quickly Boosting Decision Trees – Pruning Underachieving Features Early," ICML 2013.
 
